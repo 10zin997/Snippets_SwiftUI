358647ab9787fea10a075ebb133286d4c89fd81e
Tenzin wangyal <tenzinwangyal20@yahoo.com>
2023-05-05 12:35:19 -0700

    no message

--- BridgeUIKitSwiftUI/SwiftUIinUIKIT/SwiftUIinUIKIT/UIKitView.swift
+++ BridgeUIKitSwiftUI/SwiftUIinUIKIT/SwiftUIinUIKIT/UIKitView.swift
@@ -12,18 +12,38 @@ class UIKitView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
-        view.addSubview(button)
-        button.center = view.center
-        button.setTitle("Go to SwiftUI", for: .normal)
-        button.backgroundColor = .systemCyan
-        button.layer.cornerRadius = 15
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+//       let button = UIButton(frame: CGRect(x: 0, y: 0, width: 220, height: 50))
+//        view.addSubview(button)
+//        button.center = view.center
+//        button.setTitle("Go to SwiftUI", for: .normal)
+//        button.backgroundColor = .systemCyan
+//        button.layer.cornerRadius = 15
+//        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        self.navigationController?.navigationBar.isHidden = true
+        extractView()
     }
-    @objc func didTapButton(){
+//    @objc func didTapButton(){
+//        let vc = UIHostingController(rootView: SwiftUIView())
+//        present(vc,animated: true)
+//
+//    }
+    func extractView(){
         let vc = UIHostingController(rootView: SwiftUIView())
-        present(vc,animated: true)
-
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(vc.view)
+        
+        let constraints = [
+            vc.view.topAnchor.constraint(equalTo: view.topAnchor),
+            vc.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            vc.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            vc.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            vc.view.heightAnchor.constraint(equalTo: view.heightAnchor),
+            vc.view.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ]
+        
+        self.view.addConstraints(constraints)
     }
 
 
